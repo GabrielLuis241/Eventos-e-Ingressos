2d6ffd7e9cbb037b4d7ac3b6e8c80cd32cc5c63c
Alice Maria <alicesantos@acad.ifma.edu.br>
2025-11-26 14:11:48 -0300

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,11 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('api/usuarios/', include('usuarios.urls')),
     path('api/eventos/', include('eventos_app.urls')),
     path('api/pagamentos/', include('pagamentos.urls')),
 ]
+
