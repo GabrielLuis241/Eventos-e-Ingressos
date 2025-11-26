b97bff238f189c8800b22a7bef71fb6be40aff08
Alice Maria <alicesantos@acad.ifma.edu.br>
2025-11-26 14:13:33 -0300

    Update urls.py

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import registrar, login_view, perfil
+from .views import registrar, perfil
 
 urlpatterns = [
     path('registrar/', registrar, name='registrar_usuario'),
-    path('login/', login_view, name='login_usuario'),
     path('perfil/', perfil, name='perfil_usuario'),
 ]
